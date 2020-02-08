14e0173d007d91290392079d756edc597c59c234
ricardo-afonso <ricardoafonso.gd@gmail.com>
2020-02-08 21:59:59 +0000

    VueX Store, fetch transcriptions

--- unbabel-frontend-challenge/src/components/NavBar.vue
+++ unbabel-frontend-challenge/src/components/NavBar.vue
@@ -1,10 +1,18 @@
 <template>
   <nav>
-    Navbar goes here
+    Transcriptions
+    <BaseIcon name="upload" clickable @click.native="uploadTranscriptions()" />
+    <BaseIcon
+      name="fetch-document"
+      clickable
+      @click.native="fetchTranscriptions()"
+    />
   </nav>
 </template>
 
 <script>
+  import { mapActions } from 'vuex'
+
   export default {
     name: 'Navbar',
     components: {},
@@ -14,7 +22,10 @@
       }
     },
     methods: {
-      metohodName() {}
+      ...mapActions('transcriptions', [
+        'fetchTranscriptions',
+        'uploadTranscriptions'
+      ])
     }
   }
 </script>
---
+++ unbabel-frontend-challenge/src/store/modules/transcriptions.js
@@ -0,0 +1,46 @@
+import TranscriptionService from '@/api/TranscriptionService.js'
+
+export const namespaced = true
+
+export const state = {
+  transcriptions: [],
+  loading: false
+}
+
+export const mutations = {
+  LOAD_TRANSCRIPTIONS(state, data) {
+    state.transcriptions.push(data)
+  },
+  ADD_SINGLE_TRANSCRIPTION(state) {
+    state.transcriptions.push({
+      id: Math.floor(100 + Math.random() * 10000000) //TODO: Fetch last item's ID and ++
+    })
+  },
+  EDIT_TRANSCRIPTION(state, { index, field, text }) {
+    state.transcriptions[index][field] = text
+  },
+  // DELETE_TRANSCRIPTION (state, {index}) {
+  //   state.transcriptions.filter(index thing)
+  // },
+  TOGGLE_LOADING(state) {
+    state.loading = !state.loading
+  }
+}
+
+export const actions = {
+  fetchTranscriptions({ commit }) {
+    console.log('CALLED TRANS')
+    commit('TOGGLE_LOADING')
+    TranscriptionService.getTranscriptions()
+      .then(response => {
+        commit('LOAD_TRANSCRIPTIONS', response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+  uploadTranscriptions() {
+    console.log('uploading')
+    TranscriptionService.postTranscriptions(state.transcriptions)
+  }
+}
